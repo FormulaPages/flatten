b0e4308272929b0e0462385eaa9aeb2869a1b5ca
Peter Moresi <peter.moresi@gmail.com>
2015-09-12 20:32:59 -0700

    v2.0.0 - Replace for loop with reduce function.
    This version does not cover n-levels of depth but it does
    work on tables which are the primary use-case for spreadsheets.

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
 (function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(["exports", "module"], factory);
-    } else if (typeof exports !== "undefined" && typeof module !== "undefined") {
-        factory(exports, module);
+    if (typeof define === 'function' && define.amd) {
+        define(['exports', 'module', 'formula-isarray'], factory);
+    } else if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
+        factory(exports, module, require('formula-isarray'));
     } else {
         var mod = {
             exports: {}
         };
-        factory(mod.exports, mod);
+        factory(mod.exports, mod, global.ISARRAY);
         global.FLATTEN = mod.exports;
     }
-})(this, function (exports, module) {
-    "use strict";
+})(this, function (exports, module, _formulaIsarray) {
+    'use strict';
 
     module.exports = FLATTEN;
 
+    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+    var _ISARRAY = _interopRequireDefault(_formulaIsarray);
+
     function FLATTEN(array) {
-        var a = array;
-        if (a.constructor.name === "range") {
-            var a = a.values();
-        }
-
-        var r = [];
-
-        function _flatten(a) {
-            for (var i = 0; i < a.length; i++) {
-                if (typeof a[i] === "undefined" || a[i] === null) {
-                    continue; // empty cell or variable...just keep rolling
-                } else if (a[i].constructor.name === "range") {
-                    _flatten(a[i].values());
-                } else if (a[i].constructor == Array) {
-                    _flatten(a[i]);
-                } else {
-                    r.push(a[i]);
-                }
+        return array.reduce(function (a, b) {
+            if ((0, _ISARRAY['default'])(a)) {
+                return a.concat(b);
+            } else {
+                return [a].concat(b);
             }
-            return r;
-        }
-
-        return _flatten(a);
+        });
     }
 });
